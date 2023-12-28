6b3cfc5bb3f3574f513c283e31ff753752725f28
daniel raffel <daniel.raffel@gmail.com>
2023-12-28 02:05:45 -0800

    Update setup.py
    moved to using File Provisioner from Terraform since the startup script approach was flawed

--- setup.py
+++ setup.py
@@ -8,28 +8,45 @@ webhook_url = f"https://{n8n_hostname}/"
 fastapi_docker_image = "tiangolo/uvicorn-gunicorn-fastapi:python3.11"
 region = "us-west1"
 ssh_key = "user_name:ssh-rsa string" # Required. Example: service_account:ssh-rsa SDqhy5jXUv3xKGhzYJzjALiHg6ZzWKSSrhbjXVAvp6SecWdZPkGw16UhHHTCHvD4bwjnH6NXjHtyuCVqhdDuY1+E1BSdf0G0rncN8qFrzT1imJqraru38UEJRTZFrXMG6Kvx698J[ELvapEXXMv52zW6ZwHuU5aJ0t2atDHEXha7V3UAKSbgxLbbtQGRgtANcz3fvk9ve8GVPEtB3Cyz3eyg4aBHVqLyxx3N9hithMe
+ssh_private_key_path = "/Users/danielraffel/.ssh/gcp" # Required. Update to your private key path
+ssh_user = "daniel_raffel" # Required. Update to your SSH key user_name
 
 def fetch_project_id():
     # Fetch project ID using Google Cloud CLI
     result = subprocess.run(["gcloud", "config", "get-value", "project"], capture_output=True, text=True)
     return result.stdout.strip()
 
-def fetch_service_account_key(project_id):
+def fetch_service_account_key():
     # Fetch service account details
     accounts = subprocess.run(["gcloud", "iam", "service-accounts", "list", "--format=json"], capture_output=True, text=True)
     accounts_json = json.loads(accounts.stdout)
-    
-    # Assuming the first account is the default service account
-    default_service_account = accounts_json[0]["email"]
+
+    # Look for the Compute Engine default service account
+    compute_engine_service_account = None
+    for account in accounts_json:
+        if 'Compute Engine default service account' in account.get('displayName', ''):
+            compute_engine_service_account = account["email"]
+            break
+
+    if not compute_engine_service_account:
+        print("Compute Engine default service account not found.")
+        return None
 
     # Creating a service account key
-    key_filename = f"{project_id}-key.json"
-    subprocess.run(["gcloud", "iam", "service-accounts", "keys", "create", key_filename, "--iam-account", default_service_account])
+    key_filename = f"service-account-key.json"
+    create_key_result = subprocess.run(
+        ["gcloud", "iam", "service-accounts", "keys", "create", key_filename, "--iam-account", compute_engine_service_account],
+        capture_output=True, text=True
+    )
 
-    # Rename the key file to something generic
-    generic_key_filename = "service-account-key.json"
-    os.rename(key_filename, generic_key_filename)
-    return generic_key_filename
+    if create_key_result.returncode != 0:
+        # Handle error in key creation
+        print("Error creating service account key:", create_key_result.stderr)
+        return None
+
+    return key_filename
+
+credentials_path = fetch_service_account_key()
 
 def format_hostname(hostname):
     # Format hostname to comply with GCP naming conventions
@@ -39,31 +56,33 @@ def check_static_ip(hostname, region):
     formatted_hostname = format_hostname(hostname)
     # Check if the static IP exists
     result = subprocess.run(["gcloud", "compute", "addresses", "list", "--filter=NAME=" + formatted_hostname + " AND region:" + region, "--format=json"], capture_output=True, text=True)
+    
     if result.returncode != 0:
         # Handle error in listing IPs
         print("Error listing static IPs:", result.stderr)
-        return None
+        return None, None
 
     addresses = json.loads(result.stdout)
     for address in addresses:
-        if address["name"] == formatted_hostname and address["status"] == "RESERVED":
-            return address["address"]
+        if address["name"] == formatted_hostname:
+            # Return the IP address and the formatted hostname
+            return address["address"], formatted_hostname
 
-    # If no static IP, create one in the same region as the VM instance
+    # If no static IP, create one
     create_result = subprocess.run(["gcloud", "compute", "addresses", "create", formatted_hostname, "--region", region, "--network-tier", "STANDARD"], capture_output=True, text=True)
     if create_result.returncode != 0:
         # Handle error in creating IP
         print("Error creating static IP:", create_result.stderr)
-        return None
+        return None, None
 
     new_address_result = subprocess.run(["gcloud", "compute", "addresses", "describe", formatted_hostname, "--region", region, "--format=json"], capture_output=True, text=True)
     if new_address_result.returncode != 0:
         # Handle error in describing new IP
         print("Error describing new static IP:", new_address_result.stderr)
-        return None
+        return None, None
 
     new_address = json.loads(new_address_result.stdout)
-    return new_address["address"]
+    return new_address["address"], formatted_hostname
 
 def check_firewall_rule(project_id, rule_name):
     # Check if the firewall rule exists
@@ -92,6 +111,7 @@ def generate_terraform_config(project_id, static_ip, credentials_path):
         }
         '''
 
+    # Terraform configuration with file provisioners, remote-exec provisioner, and connection block
     config = f"""
     # Terraform configuration for setting up an instance in GCP
     provider "google" {{
@@ -116,107 +136,44 @@ def generate_terraform_config(project_id, static_ip, credentials_path):
                 network_tier = "STANDARD"
             }}
         }}
-
-        # Create setup server script to install docker, n8n and fastAPI
-        metadata_startup_script = <<-EOT
-        cat <<EOSS > /opt/setup_server.sh
-        #!/bin/bash
-        apt update
-        apt -y install docker.io apache2 wget
-        systemctl start docker
-        systemctl enable docker
-        docker pull {fastapi_docker_image}
-        docker run -d -p 8000:8000 {fastapi_docker_image}
-        docker pull n8nio/n8n
-        docker run -d -p 5678:5678 --env N8N_HOST="{n8n_hostname}" --env WEBHOOK_URL="https://{n8n_hostname}/" n8nio/n8n
-        # Install Docker Compose
-        sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-        # Verify installation
-        echo "Docker Compose version:"
-        /usr/local/bin/docker-compose --version
-        EOSS
-
-        # Check if docker-compose.yml already exists
-        if [ ! -f /opt/docker-compose.yml ]; then
-            echo "version: '1'" > /opt/docker-compose.yml
-            echo "services:" >> /opt/docker-compose.yml
-            echo "  n8n:" >> /opt/docker-compose.yml
-            echo "    image: n8nio/n8n" >> /opt/docker-compose.yml
-            echo "    ports:" >> /opt/docker-compose.yml
-            echo "      - \"5678:5678\"" >> /opt/docker-compose.yml
-            echo "    environment:" >> /opt/docker-compose.yml
-            echo "      - N8N_HOST={n8n_hostname}" >> /opt/docker-compose.yml
-            echo "      - WEBHOOK_URL={webhook_url}" >> /opt/docker-compose.yml
-            echo "    restart: unless-stopped" >> /opt/docker-compose.yml
-            echo "    volumes:" >> /opt/docker-compose.yml
-            echo "      - n8n_data:/root/.n8n" >> /opt/docker-compose.yml
-            echo "  fastapi:" >> /opt/docker-compose.yml
-            echo "    image: {fastapi_docker_image}" >> /opt/docker-compose.yml
-            echo "    ports:" >> /opt/docker-compose.yml
-            echo "      - \"8000:8000\"" >> /opt/docker-compose.yml
-            echo "    restart: unless-stopped" >> /opt/docker-compose.yml
-            echo "volumes:" >> /opt/docker-compose.yml
-            echo "  n8n_data:" >> /opt/docker-compose.yml
-        fi
-
-        # Create docker-compose service file
-        cat <<EDCS > /etc/systemd/system/docker-compose.service
-        [Unit]
-        Description=Docker Compose Application Service
-        Requires=docker.service
-        After=docker.service
-
-        [Service]
-        Type=simple
-        WorkingDirectory=/opt
-        ExecStart=/usr/local/bin/docker-compose -f /opt/docker-compose.yml up
-        ExecStop=/usr/local/bin/docker-compose -f /opt/docker-compose.yml down
-        Restart=always
-        RestartSec=5s
-
-        [Install]
-        WantedBy=multi-user.target
-        EDCS
-
-        systemctl daemon-reload
-        systemctl enable docker-compose.service
-        systemctl start docker-compose.service
-        EOF
-        chmod +x /opt/setup_server.sh
-
-        # Cloudflare setup script
-        cat <<EOCF > /opt/setup_cloudflare.sh
-        #!/bin/bash
-        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-        mv ./cloudflared-linux-amd64 /usr/local/bin/cloudflared
-        chmod a+x /usr/local/bin/cloudflared
-        cloudflared update
-        sudo cloudflared tunnel login
-        sudo cloudflared tunnel create {formatted_hostname}
-        sudo cloudflared tunnel route ip add {static_ip}/32 {formatted_hostname}
-        sudo cloudflared tunnel route dns {formatted_hostname} {n8n_hostname}
-        tunnel_id=\$(sudo cloudflared tunnel info {formatted_hostname} | grep -oP 'Your tunnel \K([a-z0-9-]+)')
-        mkdir /etc/cloudflared
-        echo "tunnel: {formatted_hostname}" > /etc/cloudflared/config.yml
-        echo "credentials-file: /root/.cloudflared/\$tunnel_id.json" >> /etc/cloudflared/config.yml
-        echo "protocol: quic" >> /etc/cloudflared/config.yml
-        echo "logfile: /var/log/cloudflared.log" >> /etc/cloudflared/config.yml
-        echo "loglevel: debug" >> /etc/cloudflared/config.yml
-        echo "transport-loglevel: info" >> /etc/cloudflared/config.yml
-        echo "ingress:" >> /etc/cloudflared/config.yml
-        echo "  - hostname: {n8n_hostname}" >> /etc/cloudflared/config.yml
-        echo "    service: http://localhost:5678" >> /etc/cloudflared/config.yml
-        echo "  - service: http_status:404" >> /etc/cloudflared/config.yml
-        cloudflared service install
-        systemctl start cloudflared
-        systemctl status cloudflared
-        EOCF
-        chmod +x /opt/setup_cloudflare.sh
-        EOT
-
         metadata = {{
             "ssh-keys" = "{ssh_key}"
+        }}
+
+        connection {{
+            type        = "ssh"
+            user        = "{ssh_user}"
+            private_key = file("{ssh_private_key_path}")
+            host        = self.network_interface[0].access_config[0].nat_ip
+        }}
+
+        # File provisioners to copy setup scripts and docker-compose files to a temporary location
+        provisioner "file" {{
+            source      = "setup_server.sh"
+            destination = "/tmp/setup_server.sh"
+        }}
+        provisioner "file" {{
+            source      = "setup_cloudflare.sh"
+            destination = "/tmp/setup_cloudflare.sh"
+        }}
+        provisioner "file" {{
+            source      = "docker-compose.yml"
+            destination = "/tmp/docker-compose.yml"
+        }}
+        provisioner "file" {{
+            source      = "docker-compose.service"
+            destination = "/tmp/docker-compose.service"
+        }}
+
+        # Remote-exec provisioner to move files to their final locations
+        provisioner "remote-exec" {{
+            inline = [
+                "sudo mv /tmp/setup_server.sh /opt/setup_server.sh",
+                "sudo chmod +x /opt/setup_server.sh",
+                "sudo mv /tmp/setup_cloudflare.sh /opt/setup_cloudflare.sh",
+                "sudo mv /tmp/docker-compose.yml /opt/docker-compose.yml",
+                "sudo mv /tmp/docker-compose.service /etc/systemd/system/docker-compose.service",
+            ]
         }}
     }}
 
@@ -233,10 +190,107 @@ def generate_terraform_config(project_id, static_ip, credentials_path):
 project_id = fetch_project_id()
 
 # Download service account key and get the path
-credentials_path = fetch_service_account_key(project_id)
+credentials_path = fetch_service_account_key()
 
 # Check for an existing static IP or create a new one
-static_ip = check_static_ip(n8n_hostname, region)
+static_ip, formatted_hostname = check_static_ip(n8n_hostname, region)
+
+# Ensure that static_ip and formatted_hostname are valid before proceeding
+if static_ip is None or formatted_hostname is None:
+    print("Error: Unable to obtain static IP or formatted hostname.")
+    exit(1)
+
+def create_file(file_name, content):
+    with open(file_name, "w") as file:
+        file.write(content)
+
+# Create setup_server.sh
+create_file("setup_server.sh", """
+#!/bin/bash
+apt update
+apt -y install docker.io apache2 wget
+systemctl start docker
+systemctl enable docker
+docker pull {fastapi_docker_image}
+docker run -d -p 8000:8000 {fastapi_docker_image}
+docker pull n8nio/n8n
+docker run -d -p 5678:5678 --env N8N_HOST="{n8n_hostname}" --env WEBHOOK_URL="{webhook_url}" n8nio/n8n
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# Verify installation
+echo "Docker Compose version:"
+/usr/local/bin/docker-compose --version
+""".format(fastapi_docker_image=fastapi_docker_image, n8n_hostname=n8n_hostname, webhook_url=webhook_url))
+
+create_file("setup_cloudflare.sh", """
+#!/bin/bash
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+mv ./cloudflared-linux-amd64 /usr/local/bin/cloudflared
+chmod a+x /usr/local/bin/cloudflared
+cloudflared update
+sudo cloudflared tunnel login
+sudo cloudflared tunnel create {formatted_hostname}
+sudo cloudflared tunnel route ip add {static_ip}/32 {formatted_hostname}
+sudo cloudflared tunnel route dns {formatted_hostname} {n8n_hostname}
+tunnel_id=\$(sudo cloudflared tunnel info {formatted_hostname} | grep -oP 'Your tunnel \K([a-z0-9-]+)')
+mkdir /etc/cloudflared
+echo "tunnel: {formatted_hostname}" > /etc/cloudflared/config.yml
+echo "credentials-file: /root/.cloudflared/\$tunnel_id.json" >> /etc/cloudflared/config.yml
+echo "protocol: quic" >> /etc/cloudflared/config.yml
+echo "logfile: /var/log/cloudflared.log" >> /etc/cloudflared/config.yml
+echo "loglevel: debug" >> /etc/cloudflared/config.yml
+echo "transport-loglevel: info" >> /etc/cloudflared/config.yml
+echo "ingress:" >> /etc/cloudflared/config.yml
+echo "  - hostname: {n8n_hostname}" >> /etc/cloudflared/config.yml
+echo "    service: http://localhost:5678" >> /etc/cloudflared/config.yml
+echo "  - service: http_status:404" >> /etc/cloudflared/config.yml
+cloudflared service install
+systemctl start cloudflared
+systemctl status cloudflared
+""".format(formatted_hostname=formatted_hostname, static_ip=static_ip, n8n_hostname=n8n_hostname))
+
+# Create docker-compose.yml
+create_file("docker-compose.yml", """
+version: '1'
+services:
+  n8n:
+    image: n8nio/n8n
+    ports:
+      - "5678:5678"
+    environment:
+      - N8N_HOST={n8n_hostname}
+      - WEBHOOK_URL={webhook_url}
+    restart: unless-stopped
+    volumes:
+      - n8n_data:/root/.n8n
+  fastapi:
+    image: {fastapi_docker_image}
+    ports:
+      - "8000:8000"
+    restart: unless-stopped
+volumes:
+  n8n_data:
+""".format(fastapi_docker_image=fastapi_docker_image, n8n_hostname=n8n_hostname, webhook_url=webhook_url))
+
+# Create docker-compose.service
+create_file("docker-compose.service", """
+[Unit]
+Description=Docker Compose Application Service
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=simple
+WorkingDirectory=/opt
+ExecStart=/usr/local/bin/docker-compose -f /opt/docker-compose.yml up
+ExecStop=/usr/local/bin/docker-compose -f /opt/docker-compose.yml down
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+""")
 
 # Generate Terraform configuration
 generate_terraform_config(project_id, static_ip, credentials_path)
