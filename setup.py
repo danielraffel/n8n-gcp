e7d12b54a3284c39d244385a51761bcfba69ec4e
daniel raffel <daniel.raffel@gmail.com>
2024-01-03 10:18:24 -0800

    Update setup.py
    Numerous refinements
    - Removed Apache2
    - Removed opening port 5678 (not required when using Cloudflare Tunnel)
    - To make updating easier switched to using [Docker's apt repository](https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository) for docker-compose plugin and regular apt repository for Cloudflared
    - Created an updater script called updater.sh which updates docker.io, docker-compose-plugin, cloudflared, n8n and FastAPI
    - Uploads updater.sh to /opt/updater.sh
    - Switched to using Docker Compose Plugin (instead of docker-compose)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 # Global variables
 n8n_hostname = "n8n.YOURDOMAIN.COM" # Required. Example: n8n.generouscorp.com
-webhook_url = f"https://{n8n_hostname}/"
+webhook_url = f"https://{n8n_hostname}/" # Note: this subdomain is added to your DNS when you configure Cloudflare Tunnnel via setup_cloudflare.sh
 fastapi_docker_image = "tiangolo/uvicorn-gunicorn-fastapi:python3.11"
 region = "us-west1"
 ssh_key = "user_name:ssh-rsa string" # Required. Example: service_account:ssh-rsa SDqhy5jXUv3xKGhzYJzjALiHg6ZzWKSSrhbjXVAvp6SecWdZPkGw16UhHHTCHvD4bwjnH6NXjHtyuCVqhdDuY1+E1BSdf0G0rncN8qFrzT1imJqraru38UEJRTZFrXMG6Kvx698J[ELvapEXXMv52zW6ZwHuU5aJ0t2atDHEXha7V3UAKSbgxLbbtQGRgtANcz3fvk9ve8GVPEtB3Cyz3eyg4aBHVqLyxx3N9hithMe
@@ -84,104 +84,84 @@ def check_static_ip(hostname, region):
     new_address = json.loads(new_address_result.stdout)
     return new_address["address"], formatted_hostname
 
-def check_firewall_rule(project_id, rule_name):
-    # Check if the firewall rule exists
-    result = subprocess.run(["gcloud", "compute", "firewall-rules", "describe", rule_name, "--project", project_id], capture_output=True)
-    return result.returncode == 0
-
 def generate_terraform_config(project_id, static_ip, credentials_path):
     formatted_hostname = format_hostname(n8n_hostname)
 
-    # Check if the firewall rule for port 5678 exists
-    firewall_rule_exists = check_firewall_rule(project_id, "allow-n8n-port")
-
-    # Conditional configuration for firewall rule
-    firewall_config = ""
-    if not firewall_rule_exists:
-        firewall_config = '''
-        # Firewall rule to allow traffic on port 5678
-        resource "google_compute_firewall" "allow_n8n_port" {
-            name    = "allow-n8n-port"
-            network = "default"
-            allow {
-                protocol = "tcp"
-                ports    = ["5678"]
-            }
-            source_ranges = ["0.0.0.0/0"]
-        }
-        '''
-
     # Terraform configuration with file provisioners, remote-exec provisioner, and connection block
-    config = f"""
-    # Terraform configuration for setting up an instance in GCP
-    provider "google" {{
-        project     = "{project_id}"
-        region      = "{region}"
-        credentials = "{credentials_path}"
-    }}
-    resource "google_compute_instance" "{formatted_hostname}" {{
-        name         = "{formatted_hostname}"
-        machine_type = "e2-micro"
-        zone         = "{region}-a"
-        boot_disk {{
-            initialize_params {{
-                image = "ubuntu-os-cloud/ubuntu-2204-lts"
-                size  = 60
-            }}
-        }}
-        network_interface {{
-            network = "default"
-            access_config {{
-                nat_ip = "{static_ip}"
-                network_tier = "STANDARD"
-            }}
-        }}
-        metadata = {{
-            "ssh-keys" = "{ssh_key}"
-        }}
-
-        connection {{
-            type        = "ssh"
-            user        = "{ssh_user}"
-            private_key = file("{ssh_private_key_path}")
-            host        = self.network_interface[0].access_config[0].nat_ip
-        }}
-
-        # File provisioners to copy setup scripts and docker-compose files to a temporary location
-        provisioner "file" {{
-            source      = "setup_server.sh"
-            destination = "/tmp/setup_server.sh"
-        }}
-        provisioner "file" {{
-            source      = "setup_cloudflare.sh"
-            destination = "/tmp/setup_cloudflare.sh"
-        }}
-        provisioner "file" {{
-            source      = "docker-compose.yml"
-            destination = "/tmp/docker-compose.yml"
-        }}
-        provisioner "file" {{
-            source      = "docker-compose.service"
-            destination = "/tmp/docker-compose.service"
-        }}
-
-        # Remote-exec provisioner to move files to their final locations
-        provisioner "remote-exec" {{
-            inline = [
-                "sudo mv /tmp/setup_server.sh /opt/setup_server.sh",
-                "sudo chmod +x /opt/setup_server.sh",
-                "sudo mv /tmp/setup_cloudflare.sh /opt/setup_cloudflare.sh",
-                "sudo mv /tmp/docker-compose.yml /opt/docker-compose.yml",
-                "sudo mv /tmp/docker-compose.service /etc/systemd/system/docker-compose.service",
-            ]
+    config = f"""# Terraform configuration for setting up an instance in GCP
+provider "google" {{
+    project     = "{project_id}"
+    region      = "{region}"
+    credentials = "{credentials_path}"
+}}
+resource "google_compute_instance" "{formatted_hostname}" {{
+    name         = "{formatted_hostname}"
+    machine_type = "e2-micro"
+    zone         = "{region}-a"
+    boot_disk {{
+        initialize_params {{
+            image = "ubuntu-os-cloud/ubuntu-2204-lts"
+            size  = 60
         }}
     }}
-
-    output "instance_ip" {{
-        value = "{static_ip}"
+    network_interface {{
+        network = "default"
+        access_config {{
+            nat_ip = "{static_ip}"
+            network_tier = "STANDARD"
+        }}
     }}
-    {firewall_config}
-    """
+    metadata = {{
+        "ssh-keys" = "{ssh_key}"
+    }}
+
+    connection {{
+        type        = "ssh"
+        user        = "{ssh_user}"
+        private_key = file("{ssh_private_key_path}")
+        host        = self.network_interface[0].access_config[0].nat_ip
+    }}
+
+    # File provisioners to copy setup scripts and docker-compose files to a temporary location
+    provisioner "file" {{
+        source      = "setup_server.sh"
+        destination = "/tmp/setup_server.sh"
+    }}
+    provisioner "file" {{
+        source      = "setup_cloudflare.sh"
+        destination = "/tmp/setup_cloudflare.sh"
+    }}
+    provisioner "file" {{
+        source      = "docker-compose.yml"
+        destination = "/tmp/docker-compose.yml"
+    }}
+    provisioner "file" {{
+        source      = "docker-compose.service"
+        destination = "/tmp/docker-compose.service"
+    }}
+    provisioner "file" {{
+        source      = "updater.sh"
+        destination = "/tmp/updater.sh"
+    }}
+
+    # Remote-exec provisioner to move files to their final locations
+    provisioner "remote-exec" {{
+        inline = [
+            "sudo mv /tmp/setup_server.sh /opt/setup_server.sh",
+            "sudo chmod +x /opt/setup_server.sh",
+            "sudo mv /tmp/setup_cloudflare.sh /opt/setup_cloudflare.sh",
+            "sudo mv /tmp/docker-compose.yml /opt/docker-compose.yml",
+            "sudo mv /tmp/docker-compose.service /etc/systemd/system/docker-compose.service",
+            "sudo mv /tmp/updater.sh /opt/updater.sh",
+            "sudo chmod +x /opt/updater.sh",
+        ]
+    }}
+}}
+
+output "instance_ip" {{
+    value = "{static_ip}"
+}}
+"""
 
     with open("setup.tf", "w") as file:
         file.write(config)
@@ -204,23 +184,38 @@ def create_file(file_name, content):
     with open(file_name, "w") as file:
         file.write(content)
 
-# Create setup_server.sh
-create_file("setup_server.sh", """
-#!/bin/bash
-apt update
-apt -y install docker.io apache2 wget
+# Create setup_server.sh install Docker, n8n, FastAPI
+create_file("setup_server.sh", """#!/bin/bash
+# Add Docker's official GPG key
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update apt repositories
+sudo apt-get update
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Manage Docker as a non-root user
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# Start and enable Docker service
 systemctl start docker
 systemctl enable docker
+# Pull Docker images for FastAPI and n8n
 docker pull {fastapi_docker_image}
 docker pull n8nio/n8n
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-# Verify installation
-echo "Docker Compose version:"
-/usr/local/bin/docker-compose --version
+# Change to the working directory
 cd /opt
-sudo /usr/local/bin/docker-compose up -d
+# Start Docker Compose
+sudo docker compose up -d
+# Enable Docker Compose service
 systemctl enable docker-compose.service
 # Create n8n volume directory
 sudo mkdir -p /home/{ssh_user}/n8n-local-files
@@ -229,12 +224,14 @@ sudo docker volume create n8n_data
 sudo mkdir -p /home/{ssh_user}/n8n-local-files
 """.format(fastapi_docker_image=fastapi_docker_image, n8n_hostname=n8n_hostname, webhook_url=webhook_url, ssh_user=ssh_user))
 
-create_file("setup_cloudflare.sh", """
-#!/bin/bash
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-mv ./cloudflared-linux-amd64 /usr/local/bin/cloudflared
-chmod a+x /usr/local/bin/cloudflared
-cloudflared update
+create_file("setup_cloudflare.sh", """#!/bin/bash
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
 sudo cloudflared tunnel login
 sudo cloudflared tunnel create {formatted_hostname}
 sudo cloudflared tunnel route ip add {static_ip}/32 {formatted_hostname}
@@ -257,8 +254,7 @@ systemctl status cloudflared
 """.format(formatted_hostname=formatted_hostname, static_ip=static_ip, n8n_hostname=n8n_hostname))
 
 # Create docker-compose.yml
-create_file("docker-compose.yml", """
-version: '1'
+create_file("docker-compose.yml", """version: '3'
 services:
   n8n:
     image: n8nio/n8n
@@ -281,8 +277,7 @@ volumes:
 """.format(ssh_user=ssh_user, fastapi_docker_image=fastapi_docker_image, n8n_hostname=n8n_hostname, webhook_url=webhook_url))
 
 # Create docker-compose.service
-create_file("docker-compose.service", """
-[Unit]
+create_file("docker-compose.service", """[Unit]
 Description=Docker Compose Application Service
 Requires=docker.service
 After=docker.service
@@ -290,14 +285,32 @@ After=docker.service
 [Service]
 Type=simple
 WorkingDirectory=/opt
-ExecStart=/usr/local/bin/docker-compose -f /opt/docker-compose.yml up
-ExecStop=/usr/local/bin/docker-compose -f /opt/docker-compose.yml down
+ExecStart=docker compose -f /opt/docker-compose.yml up
+ExecStop=docker compose -f /opt/docker-compose.yml down
 Restart=always
 RestartSec=5s
 
 [Install]
 WantedBy=multi-user.target
 """)
+
+create_file("updater.sh", """# Update the package index
+sudo apt update
+# Upgrade Docker and Cloudflared
+sudo apt upgrade docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin cloudflared
+# Pull latest docker image for FastAPI and n8n
+docker pull {fastapi_docker_image}
+docker pull n8nio/n8n
+# Stop current setup
+sudo docker compose stop
+# Delete docker-containers (data is stored separately)
+sudo docker compose rm
+# Start Docker again
+sudo docker compose -f /opt/docker-compose.yml up -d
+""")
+
+# Generate Terraform configuration
+generate_terraform_config(project_id, static_ip, credentials_path)
 
 # Generate Terraform configuration
 generate_terraform_config(project_id, static_ip, credentials_path)
